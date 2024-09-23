c334a90c0208455513b0219942b207e424d57133
PG_practice <gebe1993shuukatsu@gmail.com>
2024-09-23 15:27:27 +0000

    push test2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
 import { App } from "./App";
-//
+//1
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
